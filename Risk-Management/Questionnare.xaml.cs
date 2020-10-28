4fbe1d8a3f64d5c5e112991fb2e1905a261d1e71
Omar Arafa <omararafa43@gmail.com>
2020-10-28 16:24:15 +0200

    First commit

---
+++ Risk-Management/Questionnare.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Risk_Management
+{
+    /// <summary>
+    /// Interaction logic for Questionnare.xaml
+    /// </summary>
+    public partial class Questionnare : UserControl
+    {
+        public Questionnare()
+        {
+            InitializeComponent();
+        }
+    }
+}
