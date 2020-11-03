420d42e2e76c03c7dba86fc256856c70a28529f3
arc.ahmedessamgharib@gmail.com <arc.ahmedessamgharib@gmail.com>
2020-11-03 06:11:24 +0200

    Creating Checkboxlist Trial

--- Risk-Management/Questionnare.xaml.cs
+++ Risk-Management/Questionnare.xaml.cs
@@ -61,78 +61,63 @@ namespace Risk_Management
 
         private void Next(object sender, RoutedEventArgs e)
         {
-
-
-            //foreach (var x in Table.RowGroups)
-            //{
-
-            //}
-
-
             //Entire Row List
             //-----------------------------------------------------------------------------------------------------------------------------
             List<string> RowContent = new List<string>();
             List<string> SerialList = new List<string>();
             List<string> TextList = new List<string>();
             //-----------------------------------------------------------------------------------------------------------------------------
-            //Combine Row
-            //String.Join(String.Empty, RowContent.ToArray());
-            //string New = string.Join(",", RowContent);
-            TableRow TableRow01 = TR01 as TableRow;
-            
+            //Getting SerialList TextList
             foreach (var RowGroup in Table.RowGroups)
             {
-                
 
-                    foreach (var Row in RowGroup.Rows)
+
+                foreach (var Row in RowGroup.Rows)
                 {
-
                     if (RowContent.Count == 2)
                     {
                         SerialList.Add(RowContent[0]);
                         TextList.Add(RowContent[1]);
                         RowContent.Clear();
                     }
-
                     else if (RowContent.Count != 2)
                     {
                         RowContent.Clear();
                     }
-
-
                     //if (CheckBox01.IsChecked == true)
                     //{
                     foreach (var Cell in Row.Cells)
+                    {
+                        // May want to start another list here in case there are multiple blocks.
+                        //BlockContent List "Paragraph"
+                        List<string> blockContent = new List<string>();
+                        //---------------------------
+                        foreach (var block in Cell.Blocks.OfType<Paragraph>())
                         {
-                            // May want to start another list here in case there are multiple blocks.
-                            //BlockContent List "Paragraph"
-                            List<string> blockContent = new List<string>();
+                            // Probably want to start another list here to which to add in the next loop.
+                            //InlineContent List "Text"
+                            List<string> inlineContent = new List<string>();
                             //---------------------------
-                            foreach (var block in Cell.Blocks.OfType<Paragraph>())
-                            {
-                                // Probably want to start another list here to which to add in the next loop.
-                                //InlineContent List "Text"
-                                List<string> inlineContent = new List<string>();
-                                //---------------------------
 
-                                foreach (var inline in block.Inlines.OfType<Run>())
-                                {
-                                    // Implement whatever in here depending the type of inline,
-                                    // such as Span, Run, InlineUIContainer, etc.
-                                    // I just assumed it was text.
-                                    inlineContent.Add(new TextRange(inline.ContentStart, inline.ContentEnd).Text);
-                                }
-                                blockContent.Add(string.Join("", inlineContent.ToArray()));
-                            }
-                            if (string.Join("", blockContent.ToArray()) != "")
+                            foreach (var inline in block.Inlines.OfType<Run>())
                             {
-                            RowContent.Add(string.Join("", blockContent.ToArray()));                          
+                                // Implement whatever in here depending the type of inline,
+                                // such as Span, Run, InlineUIContainer, etc.
+                                // I just assumed it was text.
+                                inlineContent.Add(new TextRange(inline.ContentStart, inline.ContentEnd).Text);
                             }
+                            blockContent.Add(string.Join("", inlineContent.ToArray()));
+
                         }
+                        if (string.Join("", blockContent.ToArray()) != "")
+                        {
+                            RowContent.Add(string.Join("", blockContent.ToArray()));
+                        }
+                    }
                     //}
                 }
             }
-
+            //Adding Last Row Values
             if (RowContent.Count == 2)
             {
                 SerialList.Add(RowContent[0]);
@@ -143,19 +128,71 @@ namespace Risk_Management
 
 
 
-            #region Open the pop up          
-            Window02 x = new Window02(SerialList,TextList);
-            x.Height = 1000;
-            x.Width = 600;
-            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
-            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
-            double windowWidth = x.Width;
-            double windowHeight = x.Height;
-            x.Left = (screenWidth / 2) - (windowWidth / 2);
-            x.Top = (screenHeight / 2) - (windowHeight / 2);
-            x.ShowDialog();
-            #endregion 
-            
+
+
+
+
+
+
+
+            //Getting Checkboxlist
+            List<object> CheckboxList = new List<object>();
+            foreach (var RowGroup in Table.RowGroups)
+            {
+                foreach (var Row in RowGroup.Rows)
+                {
+                    if (RowContent.Count == 2)
+                    {
+                        SerialList.Add(RowContent[0]);
+                        TextList.Add(RowContent[1]);
+                        RowContent.Clear();
+                    }
+                    else if (RowContent.Count != 2)
+                    {
+                        RowContent.Clear();
+                    }
+                    foreach (var Cell in Row.Cells)
+                    {
+                        
+                        foreach (var block in Cell.Blocks.OfType<Paragraph>())
+                        {
+                            
+                            foreach (object inline in block.Inlines.OfType<InlineUIContainer>())
+                            {
+                                CheckboxList.Add(inline);
+                            }
+                            
+                        }
+                    }
+
+
+                    //
+                    //}
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+            /* #region Open the pop up          
+             Window02 x = new Window02(SerialList,TextList);
+             x.Height = 1000;
+             x.Width = 600;
+             double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+             double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+             double windowWidth = x.Width;
+             double windowHeight = x.Height;
+             x.Left = (screenWidth / 2) - (windowWidth / 2);
+             x.Top = (screenHeight / 2) - (windowHeight / 2);
+             x.ShowDialog();
+             #endregion 
+             */
 
         }
     }
